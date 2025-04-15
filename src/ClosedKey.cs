636e796680da10b08bee1680e1f4310a6e5987c0
Alexandr Ushakov <alushderkov@gmail.com>
2025-04-15 22:53:38 +0300

    Lab3

--- src/ClosedKey.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Numerics;
-
-namespace Lab3;
-
-public record ClosedKey(BigInteger P, BigInteger Q);
--- src/OpenedKey.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Numerics;
-
-namespace Lab3;
-
-public record OpenedKey(BigInteger N, BigInteger B);
